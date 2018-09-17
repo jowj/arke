40f21fc7e044853a9f33858b2464f0d43bf0f398
jowj <me@jowj.net>
2018-09-16 20:58:09 -0500

    Update arke.py to use logging() instead of manually creating a table and printing it.

--- arke.py
+++ arke.py
@@ -1,15 +1,20 @@
-import requests, arkevars, json
+import requests, arkevars, json, logging
+
+FORMAT = "%(asctime)-15s %(clientip)s %(user)-8s %(message)s"
+logging.basicConfig(format=FORMAT)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("arke")
 
 def gather_JsonData(monitoringtargets):
     responseTable = {}
     for target in monitoringtargets:
         try:
             statuscode = requests.get(target).status_code
-            responseTable[target] = statuscode
+            logger.info(f"target: {target} statuscode: {statuscode}")
             # prints the int of the status code. Find more at httpstatusrappers.com :)
 
         except requests.ConnectionError:
-            responseTable[target] = "Failed to connect."
+            logger.info(f"target: {target} ERROR: Failure to connect.")
     
     return responseTable
 
