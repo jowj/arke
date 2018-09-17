4212f2dc80e9de75c31c90079e76ea7ec3769a18
jowj <me@jowj.net>
2018-09-16 19:40:53 -0500

    Update process to work in main monitoring function.

--- arke.py
+++ arke.py
@@ -1,13 +1,16 @@
 import requests, arkevars, json
 
-responseTable = {}
-for target in arkevars.httpTargets:
-    try:
-        statuscode = requests.get(target).status_code
-        responseTable[target] = statuscode
-        # prints the int of the status code. Find more at httpstatusrappers.com :)
+def gather_JsonData(monitoringtargets):
+    responseTable = {}
+    for target in monitoringtargets:
+        try:
+            statuscode = requests.get(target).status_code
+            responseTable[target] = statuscode
+            # prints the int of the status code. Find more at httpstatusrappers.com :)
 
-    except requests.ConnectionError:
-        print("failed to connect")
+        except requests.ConnectionError:
+            responseTable[target] = "Failed to connect."
+    
+    return responseTable
 
-jsonData = json.dumps(responseTable)
+gather_JsonData(arkevars.httpTargets)
