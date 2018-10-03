e9f1a44724cd62b498932607149179aa1f6538e5
jowj <me@jowj.net>
2018-10-02 19:14:02 -0500

    Make monitor function output structured data again.

--- arke.py
+++ arke.py
@@ -4,13 +4,18 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S
 logger = logging.getLogger("arke")
 
 def monitor_AllTargets(monitoringtargets):
+    responseTable = {}
     for target in monitoringtargets:
         try:
             statuscode = requests.get(target).status_code
             logger.info(f"target: {target} statuscode: {statuscode}")
+            responseTable[target] = statuscode
             # prints the int of the status code. Find more at httpstatusrappers.com :)
 
         except requests.ConnectionError:
             logger.warn(f"target: {target} ERROR: Failure to connect.")
+            responseTable[target] = "Failed to connect."
+    
+    return responseTable
 
 monitor_AllTargets(arkevars.httpTargets)
