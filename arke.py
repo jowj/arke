0f9952a5197810d2355e85d7796a6a38fdc7eca9
jowj <me@jowj.net>
2018-12-01 19:23:19 -0600

    Change text to be written to alerts.log

--- arke.py
+++ arke.py
@@ -47,7 +47,7 @@ while is_on:
     for key,value in results[-1].items():
         if stateChanged == True:
             errorFile = open("/shared/alerts.log", "w")
-            errorText = key + " is down." + "\n"
+            errorText = key + value  + "\n"
             errorFile.write(errorText)
             errorFile.close()
     time.sleep(60)
